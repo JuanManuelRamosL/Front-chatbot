9a9dd6c489130c08d4c9f73bf31e4342415e44d2
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-08-03 22:32:00 -0300

    nav diferente,nuevo contenedor

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,20 +5,21 @@ import "./Nav.css";
 
 function Nav() {
   return (
-      <nav className="navbar">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Chat
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/preguntas" className="nav-link">
-              Ejercicios
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Chat
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/preguntas" className="nav-link">
+            Ejercicios
+          </Link>
+        </li>
+      </ul>
+      <h1 className="titulo">IAGym</h1>
+    </nav>
   );
 }
 
