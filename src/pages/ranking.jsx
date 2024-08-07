b42ab87617b69078a949f81227b63eace645cdc3
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-08-07 18:00:02 -0300

    Estilos de rank y responsive terminados

--- src/pages/ranking.jsx
+++ src/pages/ranking.jsx
@@ -17,24 +17,28 @@ function Rank() {
   return (
     <div className="container-ranking">
       <h1 className="title-ranking">Ranking</h1>
-      <ul className="ul">
+      <ul className="ul-container">
         {sortedUsers && sortedUsers.length > 0 ? (
           sortedUsers.map((user) => (
             <div key={user.id} className="container-user">
-              <img src={user.img} alt="user picture" className="img-user" />
-              <p className="name-usuario">
-                <strong className="name-usuario">Usuario:</strong>{" "}
-                <b className="user">{user.name}</b>
-              </p>
-              <div className="container-puntaje"></div>
-              <p className="name-usuario">
-                <strong className="name-usuario">Puntaje:</strong>{" "}
-                {user.puntaje}
-              </p>
-              <p className="name-usuario">
-                <strong className="name-usuario">Resueltos:</strong>{" "}
-                {user.ejercicios_resueltos}
-              </p>
+              <div className="container-image-user">
+                <img src={user.img} alt="user picture" className="img-user" />
+              </div>
+              <div className="container-info-users-rank">
+                <p className="name-usuario">
+                  <strong className="name-usuario">Usuario:</strong>{" "}
+                  <b className="user">{user.name}</b>
+                </p>
+                <div className="container-puntaje"></div>
+                <p className="name-usuario">
+                  <strong className="name-usuario">Puntaje:</strong>{" "}
+                  {user.puntaje}
+                </p>
+                <p className="name-usuario">
+                  <strong className="name-usuario">Resueltos:</strong>{" "}
+                  {user.ejercicios_resueltos}
+                </p>
+              </div>
             </div>
           ))
         ) : (
